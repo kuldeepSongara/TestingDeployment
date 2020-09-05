d845d424fa52f06bf887b5db8d9e7d0a004923ee
kul <kuldeepsongaraz@gmail.com>
2020-09-05 23:39:53 +0530

    second

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello There!!</h1>
+        <h3>KULDEEP SONGARA</h3>
+      </div>
+    );
+  }
+}
