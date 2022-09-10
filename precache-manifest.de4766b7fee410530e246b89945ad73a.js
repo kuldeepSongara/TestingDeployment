40db7466cbf01a47b878add760b59231135babb9
Kuldeep Songara <kuldeepsongaraz@gmail.com>
2022-09-10 08:24:08 +0530

    Updates

---
+++ precache-manifest.de4766b7fee410530e246b89945ad73a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8156eafe2a578ffd0fa90228ccae13d",
+    "url": "/TestingDeployment/index.html"
+  },
+  {
+    "revision": "51612cd123b1eb1d5cfd",
+    "url": "/TestingDeployment/static/css/main.19371c7e.chunk.css"
+  },
+  {
+    "revision": "f9102eb9ad6885fc5d4f",
+    "url": "/TestingDeployment/static/js/2.4807645e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TestingDeployment/static/js/2.4807645e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51612cd123b1eb1d5cfd",
+    "url": "/TestingDeployment/static/js/main.6218fef5.chunk.js"
+  },
+  {
+    "revision": "1c6aee3d68734daafb52",
+    "url": "/TestingDeployment/static/js/runtime-main.b8ee5c4a.js"
+  }
+]);
